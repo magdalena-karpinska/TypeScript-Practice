df4f55f9f0ead73fc92e11a63f5bfa11e5d4587b
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-05-30 19:22:45 +0200

    Solves Working with Object Params

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,35 @@
-import "mocha";
-import assert from "assert";
-import { addTwoNumbers } from "./index";
+import 'mocha';
+import assert from 'assert';
+import { addTwoNumbers, addTwoNums } from './index';
 
-describe("ts tests", () => {
-    it("Should add two numbers together", () => {
-        // arrange
-        // act
-        const result1 = addTwoNumbers(2, 4);
-        const result2 = addTwoNumbers(10, 10);
-        // assert
-        assert.strictEqual(6, result1);
-        assert.strictEqual(20, result2);
-    });
+describe('ts tests', () => {
+  it('Should add two numbers together', () => {
+    // arrange
+    // act
+    const result1 = addTwoNumbers(2, 4);
+    const result2 = addTwoNumbers(10, 10);
+    // assert
+    assert.strictEqual(6, result1);
+    assert.strictEqual(20, result2);
+  });
+
+  it('Should add two numbers together from an object', () => {
+    // arrange
+    const obj1 = {
+      first: 1,
+      second: 2,
+    };
+
+    const obj2 = {
+      first: 10,
+      second: 20,
+    };
+
+    // act
+    const result1 = addTwoNums(obj1);
+    const result2 = addTwoNums(obj2);
+    // assert
+    assert.strictEqual(3, result1);
+    assert.strictEqual(30, result2);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export const addTwoNumbers = (a: number, b: number) => {
-    return a + b;
-  };
+export const addTwoNumbers = (a: number, b: number) => a + b;
+
+type Params = {
+  first: number;
+  second: number;
+};
+
+export const addTwoNums = (params: Params) => params.first + params.second;
